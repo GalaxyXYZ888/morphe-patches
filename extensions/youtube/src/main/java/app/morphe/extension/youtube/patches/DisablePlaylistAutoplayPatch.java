a87f49b3c7272f3571ae9033529cd9cccd18331b
LisoUseInAIKyrios <118716522+LisoUseInAIKyrios@users.noreply.github.com>
2026-08-28 14:23:04 +0200

    chore: Add attribution notice

--- extensions/youtube/src/main/java/app/morphe/extension/youtube/patches/DisablePlaylistAutoplayPatch.java
+++ extensions/youtube/src/main/java/app/morphe/extension/youtube/patches/DisablePlaylistAutoplayPatch.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2026 Morphe.
+ * https://github.com/MorpheApp/morphe-patches/pull/2628
+ *
+ * See the included NOTICE file for GPLv3 Section 7 terms that apply to this code.
+ */
+
 package app.morphe.extension.youtube.patches;
 
 import java.lang.reflect.Field;
